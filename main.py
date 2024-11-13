5ddf55d3b5638ef520d89eacb52d92512f8f6ce7
InfoMathias <Mathias2820@gmail.com>
2024-11-13 17:27:56 -0400

    player is displayed on the screen

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
 
     running = True
     screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while running: 
 
@@ -14,10 +20,13 @@ def main():
             running = False
 
         screen.fill([0,0,0])
+        player.draw(screen)
         pygame.display.flip()
-        print('Starting asteroids!')
-        print(f'Screen width: {SCREEN_WIDTH}')
-        print(f'Screen height: {SCREEN_HEIGHT}')
+        # print('Starting asteroids!')
+        # print(f'Screen width: {SCREEN_WIDTH}')
+        # print(f'Screen height: {SCREEN_HEIGHT}')
+
+        dt = clock.tick(60) / 1000
 
 
 
